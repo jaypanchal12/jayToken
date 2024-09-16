d7220684d4dda1b890283ff42c146bd507e1fbdc
jppanchal1 <jayp@becon.app>
2024-09-16 18:24:54 +0300

    Initial Commit

---
+++ Tokenization Demo/Tokenization_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tokenization_DemoApp.swift
+//  Tokenization Demo
+//
+//  Created by jay prakash on 16/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Tokenization_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
